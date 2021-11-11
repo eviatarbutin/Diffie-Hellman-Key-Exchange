dc84089dd593e685a6dcd2d2b967eaa359f2d207
eviatarbutin <eviatar2004@gmail.com>
2021-11-11 07:39:06 -0600

    second working version

--- client.py
+++ client.py
@@ -11,15 +11,20 @@ client_socket = socket.socket()
 client_socket.connect((IP,PORT))
 print("client connected")
 
-N = int(client_socket.recv(BUFFER))
-G = int(client_socket.recv(BUFFER))
-A = int(client_socket.recv(BUFFER))
+N = int(client_socket.recv(BUFFER).decode())
+print(f"N: {N}")
+
+G = int(client_socket.recv(BUFFER).decode())
+print(f"G: {G}")
+
+A = int(client_socket.recv(BUFFER).decode())
+print(f"A: {A}")
 
 b = random.randint(N//4,N//2)
 B = G**b%N
-client_socket.send(bytes(B))
+client_socket.send(str(B).encode())
 
-print(f"N: {N}\nG: {G}\nb:{b}\nB:{B}")
+print(f"b:{b}\nB:{B}")
 
 key = A**b%N
 print(f"A:{A}\nkey:{key}")
--- server.py
+++ server.py
@@ -6,7 +6,7 @@ IP = "0.0.0.0"
 PORT = 1337
 BUFFER = 4096
 NUMBER_OF_CLIENTS = 1
-N_NUMBER_OF_BITS = 2**100
+N_range = 4000
 print("Server is up:")
 server_socket = socket.socket()
 server_socket.bind((IP,PORT))
@@ -14,19 +14,21 @@ server_socket.listen(NUMBER_OF_CLIENTS)
 client_socket, client_address = server_socket.accept()
 print(f"{client_address} connected")
 
-N = random.randint(N_NUMBER_OF_BITS//2,N_NUMBER_OF_BITS)
+N = random.randint(N_range//2,N_range)
+client_socket.send((str(N)+"\n").encode())
 print(f"N: {N}")
+
 G = 37
 print(f"G: {G}")
+client_socket.send((str(G)+"\n").encode())
+
 a = random.randint(N//8,N//4)
 print(f"a: {a}")
 A = G**a%N
 print(f"A: {A}")
-client_socket.send(bytes(N))
-client_socket.send(bytes(G))
-client_socket.send(bytes(A))
+client_socket.send((str(A)+"\n").encode())
 
-B = int(client_socket.recv(BUFFER))
+B = int(client_socket.recv(BUFFER).decode())
 key = B**a%N
 print(f"B:{B}\nkey:{key}")
 
